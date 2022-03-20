c38994c8087950551e956d892f364e3c0de0de01
Yuri Brito <yurisb85@gmail.com>
2022-03-20 19:31:37 -0300

    Teste API SA3

--- teste.js
+++ teste.js
@@ -1,34 +1,13 @@
-function testando(string){
-    var x=eval(string)
-    return x
+var pki = new LacunaWebPKI();
+pki.init();
+var request = {
+    // URL do servico
+    url: "https://sa3.rfoc.srf/php/serv/cad_servidor.php?req=e-Bibliotecas:09884422729&chave=09884422729&dados=matSiapeCad|nomePessoa|nomeCargo",
+    method: 'get'
 }
-function troca(){
-    tabela=document.getElementById("yuri")
-    linha1=document.querySelector('body > table > tbody > tr:nth-child(1) > td:nth-child(1)').getInnerHTML()
-    linha2=document.querySelector('body > table > tbody > tr:nth-child(1) > td:nth-child(2)').getInnerHTML()
-    document.querySelector('body > table > tbody > tr:nth-child(1) > td:nth-child(1)').innerHTML=linha2
-    document.querySelector('body > table > tbody > tr:nth-child(1) > td:nth-child(2)').innerHTML=linha1
-}
-function nova(){
-      var span = document.getElementsByTagName('span')[0];
-      span.textContent = 'interactive'; // change DOM text content
-      span.style.display = 'inline';  // change CSSOM property
-      // create a new element, style it, and append it to the DOM
-      var loadTime = document.createElement('div');
-      loadTime.textContent = 'You loaded this page on: ' + new Date();
-      loadTime.style.color = 'blue';
-      document.body.appendChild(loadTime);
-}
-function tabela(){
-    let tab=document.getElementById("yuri")
-    let tab_body=document.createElement('tbody')
-    tab.appendChild(tab_body)
-    tab_tr=document.createElement("tr")
-    tab_body.appendChild(tab_tr)
-    let lista=['yuri','aline','miguel']
-    for (i in lista){
-        tab_td=document.createElement('td')
-        tab_td.textContent=lista[i]
-        tab_tr.appendChild(tab_td)
-    }
-}
+ 
+request.certificateThumbprint = $('#certificateSelect').val();
+pki.sendAuthenticatedRequest(request).success(function (response) {
+    // Usa a resposta "response"
+});
+console.log(response)
--- teste1.js
+++ teste1.js
@@ -1,6 +1,7 @@
 
 let url_token = " http://127.0.0.1:8000/token/";//Sua URL
 let url = " http://127.0.0.1:8000/catalog/registros/";//Sua URL
+let url_sa3="https://sa3.rfoc.srf/php/serv/cad_servidor.php?req=e-Bibliotecas:09884422729&chave=09884422729&dados=matSiapeCad|nomePessoa|nomeCargo"
 let xhttp = new XMLHttpRequest();
 xhttp.open("POST", url_token, false);
 xhttp.setRequestHeader('Content-Type','application/json')
@@ -10,3 +11,32 @@ xhttp.open("GET",url,false)
 xhttp.setRequestHeader('Authorization','Bearer '+senha['access'])
 xhttp.send()
 console.log(JSON.parse(xhttp.responseText));
+dados=JSON.parse(xhttp.responseText)
+let tab=document.getElementById("yuri")
+let tab_body=document.createElement('tbody')
+tab.appendChild(tab_body)
+tab_tr=document.createElement("tr")
+tab_body.appendChild(tab_tr)
+for (j in dados[0]){
+    titulo=document.createElement("td")
+    titulo.textContent=j
+    tab_tr.appendChild(titulo)
+}
+
+for (i in dados){
+    tab_tr=document.createElement("tr")
+    tab_body.appendChild(tab_tr)
+    
+    for (j in dados[i]){
+        tab_td=document.createElement('td')
+        tab_td.contentEditable=true
+        tab_td.textContent=dados[i][j]
+        tab_tr.appendChild(tab_td)
+    }}
+
+
+xhttp.open("GET","https://sa3.rfoc.srf/php/serv/cad_servidor.php?req=e-Bibliotecas:09884422729&chave=09884422729&dados=matSiapeCad|nomePessoa|nomeCargo",true)
+xhttp.withCredentials=true
+console.log(xhttp.withCredentials)
+
+xhttp.send()
